6e0fbedb5659dec216bba2ee6fff57088f1c643c
Bartłomiej Jatczak <b.jatczak@gmail.com>
2021-03-24 14:23:36 +0100

    Image rotate and scale fixes

--- LabelCreatorLib/Models/ImageObject.cs
+++ LabelCreatorLib/Models/ImageObject.cs
@@ -115,15 +115,5 @@ namespace LabelCreator
         }
 
 
-        //private Bitmap GetScaledImage(Bitmap image, int maxWidth, int maxHeight)
-        //{
-        //    var newImage = new Bitmap(maxWidth, maxHeight);
-        //    using (var graphics = Graphics.FromImage(newImage))
-        //        graphics.DrawImage(image, 0, 0, maxWidth, maxHeight);
-
-        //    return newImage;
-        //}
-
-
     }
 }
