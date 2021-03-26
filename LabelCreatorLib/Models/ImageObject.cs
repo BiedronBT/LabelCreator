abeac73694ded011bb0c17612b5f48af0c9f7201
Bartłomiej Jatczak <b.jatczak@gmail.com>
2021-03-26 13:58:07 +0100

    Rotation extended and simpified

--- LabelCreatorLib/Models/ImageObject.cs
+++ LabelCreatorLib/Models/ImageObject.cs
@@ -7,21 +7,12 @@ using System.Text;
 
 namespace LabelCreator
 {
-    public enum ImageRotation
-    {
-        None,
-        Deg90,
-        Deg180,
-        Deg270
-    }
-
     public class ImageObject : IPasteable, IBitmapConvertible
     {
 
         private Bitmap _image;
         private int _positionX;
         private int _positionY;
-        private ImageRotation _rotation = ImageRotation.None;
 
         public int Width { get; private set; }
         public int Height { get; private set; }
@@ -53,50 +44,11 @@ namespace LabelCreator
             return this;
         }
 
-        public ImageObject Rotate(ImageRotation rotation)
+        public ImageObject RotateFlip(RotateFlipType rotateFlipType)
         {
-            _rotation = rotation;
-            Bitmap newBitmap;
-
-            if (rotation == ImageRotation.None || rotation == ImageRotation.Deg180)
-                newBitmap = new Bitmap(Width, Height);
-            else
-                newBitmap = new Bitmap(Height, Width);
-
-            using (Graphics graphics = Graphics.FromImage(newBitmap))
-            {
-                int x = 0;
-                int y = 0;
-
-                switch (_rotation)
-                {
-                    case ImageRotation.None:
-                        break;
-                    case ImageRotation.Deg90:
-                        y = -Height;
-                        graphics.RotateTransform(90f);
-                        break;
-                    case ImageRotation.Deg180:
-                        x = -Width;
-                        y = -Height;
-                        graphics.RotateTransform(180f);
-                        break;
-                    case ImageRotation.Deg270:
-                        x = -Width;
-                        graphics.RotateTransform(270f);
-                        break;
-                }
-
-                graphics.DrawImage(_image,
-                                   new RectangleF(x, y, Width, Height),
-                                   new RectangleF(0, 0, Width, Height),
-                                   GraphicsUnit.Pixel);
-            }
-            _image = newBitmap;
-
+            _image.RotateFlip(rotateFlipType);
             return this;
         }
-
 
         public Bitmap GetBitmap()
         {
